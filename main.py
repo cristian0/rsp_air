9d6417eaff411683dabdbd323007bf3059a1406b
Cristiano <dabduster@gmail.com>
2024-05-24 16:18:35 +0200

    code formatted

--- main.py
+++ main.py
@@ -12,9 +12,11 @@ def log(message):
     print(f"{str(datetime.fromtimestamp(time.time()))} {message}")
     sys.stdout.flush()
 
+
 def interrupt_handler(signum, frame):
     log(f"Closing for signal {signum} ({signal.Signals(signum).name}).")
     sys.exit(0)
+
 
 def insert_sampling(aqi, tvoc, eco2, mode):
 
@@ -27,22 +29,27 @@ def insert_sampling(aqi, tvoc, eco2, mode):
 
     now = int(time.time())
 
-    cur.execute("INSERT INTO air (date, aqi, tvoc, eco2, mode) values (?, ?, ?, ?, ?)", (now, aqi, tvoc, eco2, mode))
+    cur.execute(
+        "INSERT INTO air (date, aqi, tvoc, eco2, mode) values (?, ?, ?, ?, ?)",
+        (now, aqi, tvoc, eco2, mode),
+    )
     con.commit()
     con.close()
 
+
 ###########################################################
+
 
 def main():
     log(f"Started")
 
     retry_time_for_operation = {
-        'operating ok': 60,
-        'warm-up': 10,
-        'initial start-up': 60, 
-        'no valid output': 10, 
-        'fake operating ok': 10,
-        'turned off': 10
+        "operating ok": 60,
+        "warm-up": 10,
+        "initial start-up": 60,
+        "no valid output": 10,
+        "fake operating ok": 10,
+        "turned off": 10,
     }
 
     sreader = Sample_ENS160()
@@ -50,22 +57,35 @@ def main():
     count = 0
     while True:
         sample = sreader.sample()
-        print((sample, sreader.aqi_value, sreader.tvoc, sreader.eco2_value, sreader.operation))
-        if(sample != False):
-            insert_sampling(sreader.aqi_value, sreader.tvoc, sreader.eco2_value, sreader.operation)
-            log(f"Sampled AQI:{str(sreader.aqi_value)} TVOC:{str(sreader.tvoc)}ppb eCO2:{str(sreader.eco2_value)}ppm")
+        print(
+            (
+                sample,
+                sreader.aqi_value,
+                sreader.tvoc,
+                sreader.eco2_value,
+                sreader.operation,
+            )
+        )
+        if sample != False:
+            insert_sampling(
+                sreader.aqi_value, sreader.tvoc, sreader.eco2_value, sreader.operation
+            )
+            log(
+                f"Sampled AQI:{str(sreader.aqi_value)} TVOC:{str(sreader.tvoc)}ppb eCO2:{str(sreader.eco2_value)}ppm"
+            )
         else:
             log(f"Mode: '{sreader.operation}', not sampling")
             # count += 1
             # if(sreader.operation == 'no valid output' and count == 3) :
             #     log(f"rebooting reader")
             #     count = 0
-                # del(sreader)
-                # sreader = Sample_ENS160()
+            # del(sreader)
+            # sreader = Sample_ENS160()
 
         time.sleep(retry_time_for_operation[sreader.operation])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     signal.signal(signal.SIGINT, interrupt_handler)
 
     main()
--- web/main.py
+++ web/main.py
@@ -26,83 +26,89 @@ def db_to_dataframe(db_file, table_name):
         conn.close()
         return df
 
-
     except sqlite3.Error as e:
         print(f"Error connecting to database or reading table: {e}")
 
+
 def remove_iqr_outliers_multi(df, columns_to_check, threshold=1.1):
-  """
-  Removes outliers from multiple columns in a DataFrame based on IQR.
+    """
+    Removes outliers from multiple columns in a DataFrame based on IQR.
 
-  Args:
-      df (pd.DataFrame): The DataFrame containing the data.
-      columns_to_check (list): A list of column names to check for outliers.
-      threshold (float, optional): The IQR multiplier for outlier detection (default: 1.5).
+    Args:
+        df (pd.DataFrame): The DataFrame containing the data.
+        columns_to_check (list): A list of column names to check for outliers.
+        threshold (float, optional): The IQR multiplier for outlier detection (default: 1.5).
 
-  Returns:
-      pd.DataFrame: A new DataFrame with outliers removed from the specified columns.
-  """
+    Returns:
+        pd.DataFrame: A new DataFrame with outliers removed from the specified columns.
+    """
 
-  filtered_df = df.copy()  # Copy the DataFrame to avoid modifying the original
-  for col in columns_to_check:
-    Q1 = filtered_df[col].quantile(0.10)
-    Q3 = filtered_df[col].quantile(0.90)
-    IQR = Q3 - Q1
-    lower_bound = Q1 - threshold * IQR
-    upper_bound = Q3 + threshold * IQR
-    filtered_df = filtered_df[(filtered_df[col] >= lower_bound) & (filtered_df[col] <= upper_bound)]
-  return filtered_df
+    filtered_df = df.copy()  # Copy the DataFrame to avoid modifying the original
+    for col in columns_to_check:
+        Q1 = filtered_df[col].quantile(0.10)
+        Q3 = filtered_df[col].quantile(0.90)
+        IQR = Q3 - Q1
+        lower_bound = Q1 - threshold * IQR
+        upper_bound = Q3 + threshold * IQR
+        filtered_df = filtered_df[
+            (filtered_df[col] >= lower_bound) & (filtered_df[col] <= upper_bound)
+        ]
+    return filtered_df
+
 
 def get_formatted_data(**kwargs):
 
-    last_hours = kwargs['last_hours'] if 'last_hours' in kwargs else 24
-    
-    df = db_to_dataframe('../air.db', 'meteo_samples')
-    one_hour = (60 * 60)
+    last_hours = kwargs["last_hours"] if "last_hours" in kwargs else 24
+
+    df = db_to_dataframe("../air.db", "meteo_samples")
+    one_hour = 60 * 60
     hours_delay = last_hours * one_hour
 
     howback_date = datetime.timestamp(datetime.now()) - hours_delay
 
-    df = df[df['date'] > howback_date]
+    df = df[df["date"] > howback_date]
 
-    df['date'] = df['date'].apply(datetime.fromtimestamp)
-    df.set_index('date', inplace=True)
+    df["date"] = df["date"].apply(datetime.fromtimestamp)
+    df.set_index("date", inplace=True)
 
-    df_reshaped = df.pivot_table(values='value', index='date', columns='metric')
+    df_reshaped = df.pivot_table(values="value", index="date", columns="metric")
 
-    cols = ['temperature','relative_humidity', 'gas', 'pressure', 'altitude']
+    cols = ["temperature", "relative_humidity", "gas", "pressure", "altitude"]
     df_no_outliers = remove_iqr_outliers_multi(df_reshaped, cols, threshold=1.5)
-    cols.insert(0, 'aqi')
-    df_no_outliers['aqi'] = np.log(df_no_outliers['gas']) + 0.04 * df_no_outliers['relative_humidity']
+    cols.insert(0, "aqi")
+    df_no_outliers["aqi"] = (
+        np.log(df_no_outliers["gas"]) + 0.04 * df_no_outliers["relative_humidity"]
+    )
     ret = {}
     for _col in cols:
         ret[_col] = df_no_outliers[_col]
         ret[_col] = ret[_col].reset_index()
-        ret[_col]['date'] = ret[_col]['date'].astype(str)
-        ret[_col] = ret[_col].to_dict(orient='records')
-        ret[_col] = [{'x': item['date'], 'y': item[_col]} for item in ret[_col]]
+        ret[_col]["date"] = ret[_col]["date"].astype(str)
+        ret[_col] = ret[_col].to_dict(orient="records")
+        ret[_col] = [{"x": item["date"], "y": item[_col]} for item in ret[_col]]
 
     return ret
 
-app = Flask(__name__)
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/meteo_data.js')
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/meteo_data.js")
 def meteo_data():
 
-    last_hours = request.args.get('last_hours', 24)
+    last_hours = request.args.get("last_hours", 24)
 
-    datasets = get_formatted_data(last_hours = int(last_hours))
+    datasets = get_formatted_data(last_hours=int(last_hours))
     resp = make_response(json.dumps(datasets), 200)
-    resp.headers['Content-Type'] = 'application/javascript; charset=utf-8'
-    resp.headers['Server'] = 'Pizza/0.1'
+    resp.headers["Content-Type"] = "application/javascript; charset=utf-8"
+    resp.headers["Server"] = "Pizza/0.1"
     return resp
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
 
-
-
-
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8000)
